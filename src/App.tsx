7009a4505b148a6e177991cbfa1269612793bb01
Joao Marcos <jmcsjoaomarcos@gmail.com>
2024-10-25 16:50:24 -0300

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="">
+      <header className="bg-green-500">
+        <p className='bg-red-500'>
+          Edit <code>src/App.tsx</code> and save to reload aaaaaaaaa.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
